53836f700c5b06839fc7e1646f1202bfe3595c3a
khuongbhan <hbk32@drexel.edu>
2025-02-17 02:44:19 -0800

    tablet n mobile base completed

--- scripts.js
+++ scripts.js
@@ -1,54 +1,28 @@
-
 var $burger = $("#burger-icon");
 // var $nav = $("#nav-container");
-var $nav = document.getElementById("#nav-container")
+var $nav = document.getElementById("nav-container")
 var $navA = $('nav[data-nav] a');
 var isVisible = false;
 
-// function mouseClick(){
-
-//     console.log("click");
-//     if(isVisible === false){
-//         $nav.show();
-//         isVisible = true;
-//     }
-//     else{
-//         $nav.hide();
-//         isVisible = false;
-//     }
-// }
-
-function myFunction(y,x) {
+function myFunction(y, x) {
     y.classList.toggle("change");
 
     console.log("concu");
-    if(isVisible === false){
+    if (isVisible === false) {
         // $nav.show();
-        x.style.display= "block"
+        x.style.display = "block"
         isVisible = true;
-    }
-    else{
+    } else {
         // $nav.hide();
-        x.style.display= "none"
+        x.style.display = "none"
         isVisible = false;
     }
 }
 
-window.onload = function() {
+window.onload = function () {
     var $burger = document.getElementById("burger-icon");
-var $nav = document.getElementById("nav-container")
-// var $navA = $('nav[data-nav] a');
-    // $navA.onclick = myFunction;
-    $burger.onclick = function() {myFunction($burger,$nav)};
-//     $nav.on( "click", myFunction);
-// $navA.on( "click", myFunction);
-// $burger.on( "click", myFunction);
+    var $nav = document.getElementById("nav-container")
+    $burger.onclick = function () {
+        myFunction($burger, $nav)
+    };
 }
-
-
-
-// $burger.on( "click", mouseClick);
-// // close the mobile menu when menu link is clicked
-// $navA.on( "click", mouseClick);
-
-
